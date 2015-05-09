9fecf352525b19df97e4ab85c7daed1882cda800
QuHarrison Terry <qu42blue@gmail.com>
2015-05-08 20:57:21 -0500

    First Comit

---
+++ NewBoston/apples.java
@@ -0,0 +1,9 @@
+class apples{
+	public static void main(String[] args){
+		tuna tunaObject = new tuna();
+		System.out.println(tunaObject.toMilitary());
+		tunaObject.setTime(13, 27, 6);
+		System.out.println(tunaObject.toMilitary());
+	}
+	
+}
